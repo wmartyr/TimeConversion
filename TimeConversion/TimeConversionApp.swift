8533265ffe05f24c60f2796deff1bdbf085d8b20
Woodrow Martyr <woodrow.martyr@gmail.com>
2023-12-23 10:47:47 +1030

    Initial Commit

---
+++ TimeConversion/TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionApp.swift
+//  TimeConversion
+//
+//  Created by Woodrow Martyr on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
